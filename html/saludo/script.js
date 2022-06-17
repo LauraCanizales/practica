78d3a58b4606673c72dc81feb4a569e340f03015
Laura Canizales <lau.cecilia93@gmail.com>
2022-06-17 18:58:04 -0500

    Se modificaron

---
+++ html/saludo/script.js
@@ -0,0 +1,20 @@
+console.log("Se nota que todavia me extranas");
+
+
+
+setInterval(cambiarSaludo, 2000);
+
+function cambiarSaludo() {
+    //console.log("Me parece una falta de respeto");
+    let frase = document.getElementsByTagName("h2")[1].innerHTML;
+
+    if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre"){
+        //frase "Impongo moda para las ninas"
+        document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las ninas";
+    } else {
+        frase = "Ganando como siempre";
+        document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+    }
+    console.log("frase: " + frase);
+
+}
