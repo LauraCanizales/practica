3021621842b02ed6007ebcb438c9259962b470db
Laura Canizales <lau.cecilia93@gmail.com>
2022-06-17 19:07:43 -0500

    Se corrigieron las frases

--- html/saludo/script.js
+++ html/saludo/script.js
@@ -8,12 +8,12 @@ function cambiarSaludo() {
     //console.log("Me parece una falta de respeto");
     let frase = document.getElementsByTagName("h2")[1].innerHTML;
 
-    if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre"){
-        //frase "Impongo moda para las ninas"
-        document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las ninas";
+    if (document.getElementsByTagName("h2")[1].innerHTML == "¡Ganando como siempre!"){
+        //frase "Impongo moda para las niñas"
+        document.getElementsByTagName("h2")[1].innerHTML = "¡Impongo moda para las niñas!";
     } else {
         frase = "Ganando como siempre";
-        document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+        document.getElementsByTagName("h2")[1].innerHTML = "¡Ganando como siempre!";
     }
     console.log("frase: " + frase);
 
